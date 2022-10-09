c790a47783908f63ba7252fd836f78d1c2cade78
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-10-09 22:45:15 +0300

    Added validations

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,4 +1,7 @@
 class HeroPower < ApplicationRecord
     belongs_to :hero
     belongs_to :power
+
+    enum strength: %i[Strong Weak Average]   # [:Strong, :Weak, :Average]
+    validates :strength, inclusion: { in: strength.keys }
 end
